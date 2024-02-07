04e1e765e6e46176b780b698642067edf260677e
Shelton Bilgates <sheltonbilgates@gmail.com>
2024-02-07 15:06:10 +0530

    banner completed

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,15 +1,8 @@
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Tab } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-// import Box from '@mui/material/Box';
-// import Tab from '@mui/material/Tab';
-// import { TabContext } from '@material-ui/lab';
-// import TabList from '@mui/lab/TabList';
-// import TabPanel from '@mui/lab/TabPanel';
-
-
-
-
-
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Banner = () => {
   const [weather, setWeather] = useState(null);
@@ -36,8 +29,8 @@ const Banner = () => {
       }
     }
   }, [getPosition]);
-  let celcius = 0;
-  let faranheit = 0;
+  // let celcius = 0;
+  // let faranheit = 0;
 
   const getWeatherData = () => {
     try {
@@ -98,7 +91,8 @@ const Banner = () => {
 
   console.log("Current City:", city);
   console.log("Weather response:", weather);
-  // console.log("Forecast response:", forecast);
+  console.log("Hourly response:", hourlyData);
+  console.log("Daily response:", dailyData);
 
   const handleuseGPS = () => {
     setGetPosition(true);
@@ -114,7 +108,9 @@ const Banner = () => {
 
           // Extracting hourly data for today
           const today = new Date().toLocaleDateString();
-          const todayHourly = allData.filter((item) => new Date(item.dt * 1000).toLocaleDateString() === today);
+          const todayHourly = allData.filter(
+            (item) => new Date(item.dt * 1000).toLocaleDateString() === today
+          );
 
           // Extracting one entry per day for the next 7 days
           const next7DaysData = [];
@@ -137,18 +133,18 @@ const Banner = () => {
     }
   }, [weather, city]);
 
-  const [value, setValue] = React.useState('1');
+  const [value, setValue] = React.useState("one");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <div className=" h-56  rounded-2xl mt-2 mr-8 ml-8 shadow-2xl">
+    <div className=" h-64  rounded-2xl mt-2 mr-8 ml-8 shadow-2xl text-white">
       <div className="w-full h-full">
-        <div className="absolute w-[95%]  rounded-2xl h-56 bg-gradient-to-l from-black "></div>
+        <div className="absolute w-[95%]  rounded-2xl h-64 bg-gradient-to-l from-gray-700 "></div>
         <img
-          className="w-screen h-56 object-cover rounded-2xl"
+          className="w-screen h-64 object-cover rounded-2xl"
           src="https://i.pinimg.com/564x/bc/37/d3/bc37d3bf4601abe71137fc9beb46c708.jpg"
           alt=""
         />
@@ -176,7 +172,7 @@ const Banner = () => {
           {weather && (
             <div>
               <div className="flex flex-row absolute gap-6  top-[20%]">
-                <div className="flex flex-col w-32 h-10 ml-4">
+                <div className="flex flex-col w-32 h-10 ">
                   <img
                     className="flex justify-center items-center"
                     src={`https://openweathermap.org/img/wn/${weather.pngID}@2x.png`}
@@ -215,46 +211,89 @@ const Banner = () => {
                     </p>
                   </div>
                 </div>
-                <div >
+                <div>
+                  <TabContext value={value}>
+                    <TabList onChange={handleChange}>
+                      <Tab label="Hourly" value="one" />
+                      <Tab label="7-day" value="two" />
+                    </TabList>
+                    <TabPanel value="one">
+                      {hourlyData.length > 0 && (
+                        <div className="flex flex-row gap-4">
+                          {hourlyData.map((item, idx) => (
+                            <div
+                              key={item.dt}
+                              className=" w-28 h-24 rounded-xl justify-center text-center items-center content-center shadow-2xl backdrop-blur-lg"
+                            >
+                              <p>
+                                {new Date(item.dt * 1000).toLocaleTimeString(
+                                  [],
+                                  {
+                                    hour: "numeric",
+                                    minute: "numeric",
+                                    hour12: true,
+                                  }
+                                )}
+                              </p>
+                              <div className="flex flex-row justify-center text-center items-center">
+                                <img
+                                  className=""
+                                  src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                                  alt=""
+                                />
+                                <p>{item.main.temp}°C</p>
+                              </div>
+                              <p className="text-center mt-[-5px]">
+                                {" "}
+                                {item.weather[0].description}
+                              </p>
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    </TabPanel>
 
-                {/* <TabContext value={value}>
-  <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-    <TabList onChange={handleChange} aria-label="lab API tabs example">
-      <Tab label="Item One" value="1" />
-      <Tab label="Item Two" value="2" />
-      <Tab label="Item Three" value="3" />
-    </TabList>
-  </Box>
-  <TabPanel value="1">Item One</TabPanel>
-  <TabPanel value="2">Item Two</TabPanel>
-  <TabPanel value="3">Item Three</TabPanel>
-</TabContext> */}
+                    {/* 7 day data  */}
 
-
-
-                {hourlyData.length > 0 && (
-          <div>
-            <h2>Hourly Forecast for Today</h2>
-            {hourlyData.map((item) => (
-              <div key={item.dt}>
-                <p>{new Date(item.dt * 1000).toLocaleTimeString()}</p>
-                {/* Add other hourly data display */}
-              </div>
-            ))}
-          </div>
-        )}
-
-        {dailyData.length > 0 && (
-          <div>
-            <h2>7-Day Forecast</h2>
-            {dailyData.map((item) => (
-              <div key={item.dt}>
-                <p>{new Date(item.dt * 1000).toLocaleDateString("en-GB")}</p>
-                {/* Add other daily data display */}
-              </div>
-            ))}
-          </div>
-        )}
+                    <TabPanel value="two">
+                      {dailyData.length > 0 && (
+                        <div className="flex flex-row gap-1">
+                          {dailyData.map((item) => (
+                            <div
+                              key={item.dt}
+                              className="w-28 h-32 rounded-xl justify-center text-center items-center content-center shadow-2xl backdrop-blur-lg"
+                            >
+                              <p>
+                                {new Date(item.dt * 1000).toLocaleDateString(
+                                  "en-GB",
+                                  {
+                                    weekday: "short",
+                                  }
+                                )}
+                              </p>
+                              <p className="text-[12px]">
+                                {new Date(item.dt * 1000).toLocaleDateString(
+                                  "en-GB"
+                                )}
+                              </p>
+                              <div className="flex flex-row justify-center text-center items-center mt-[-5px]">
+                                <img
+                                  className=""
+                                  src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                                  alt=""
+                                />
+                                <p>{item.main.temp}°C</p>
+                              </div>
+                              <p className="text-center mt-[-5px]">
+                                {" "}
+                                {item.weather[0].description}
+                              </p>
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    </TabPanel>
+                  </TabContext>
                 </div>
               </div>
             </div>
